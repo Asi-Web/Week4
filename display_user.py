2039682c857115f25811b4e56bbab764b9cdbef7
Asi-Web <agblezea@students.trinitydc.edu>
2026-02-24 13:20:44 -0500

    Ass function to display user details
    Implement display_user() function with parameters

---
+++ display_user.py
@@ -0,0 +1,14 @@
+## Asiwome Agbleze
+## CMSC 111
+## Assignment -display_user function
+
+
+# This takes a name and age as parameters and prints the user's details
+def display_user(name, age):
+    print("Name:", name)
+    print("Age:", age)
+
+
+    # Call the function with sample values so the output shows when the program runs
+    display_user("Alex", 20)
+    
